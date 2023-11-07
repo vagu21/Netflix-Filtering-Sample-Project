42eca602ed0c7d58293bad92569528e41c07de8e
vagu21 <bharadwaj21197@gmail.com>
2023-11-07 15:39:46 +0530

    movies added

---
+++ app.js
@@ -0,0 +1,184 @@
+
+
+let movieslist=[
+   {
+         name:"Temper",
+         poster:"https://i.pinimg.com/736x/8e/4e/13/8e4e131e7ee47cfbbd48b9e9ccffda23.jpg",
+         rating:8.7
+   },
+   {
+         name:"Adipurush",
+         poster:"https://i0.wp.com/www.socialnews.xyz/wp-content/uploads/2022/09/30/Rebel-Star-Prabhas-Adipurush-Movie-First-Look-HD-posters-9.jpg?fit=2000%2C2500&quality=80&zoom=1&ssl=1?v=1664503362",
+         rating:8.9
+   },
+   {
+         name:"Animal",
+         poster:"https://cdn.bollywoodmdb.com/fit-in/movies/largethumb/2021/animal/animal-poster-2.jpg",
+         rating:9.5
+
+   },
+   {
+         name:"KGF",
+         poster:"https://m.media-amazon.com/images/S/pv-target-images/f5a21f64af7359f7aaa7c29dee8a12a97630e707cdbf71e0ae6b063322fb8575.jpg",
+         rating:9.5
+   },
+   {
+         name:"RRR",
+         poster:"https://upload.wikimedia.org/wikipedia/en/d/d7/RRR_Poster.jpg",
+         rating:9.8
+   },
+   {
+         name:" Avengers Infinity War",
+         poster:"https://m.media-amazon.com/images/I/71yPvi+HMiL._AC_UF1000,1000_QL80_.jpg",
+         rating:8.3
+   },
+   {
+         name:" Avengers End Game",
+         poster:"https://lumiere-a.akamaihd.net/v1/images/p_avengersendgame_19751_e14a0104.jpeg?region=0%2C0%2C540%2C810",
+         rating:9.8
+   },
+   {
+         name:"Maharshi",
+         poster:"https://m.media-amazon.com/images/M/MV5BNzQ3YzkyMjEtNGFmYi00Y2QzLTg4NjItODMzNWQ0NzBiYWEzXkEyXkFqcGdeQXVyNjQ1MDcxNzM@._V1_.jpg",
+         rating:9.6
+   },
+   {
+         name:"Guntur Karam",
+         poster:"https://cdn.gulte.com/wp-content/uploads/2023/08/F3FPlJ4aQAA2M2W.jpeg",
+         rating:9.8
+    },
+    {
+         name:"Sarileru Nekevvaru",
+         poster:"https://m.media-amazon.com/images/M/MV5BN2FmZjBmOGYtNTJkNy00M2E3LWE1N2UtOTRlMjU1MTU1ZTkwXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg",
+         rating:"9.6"
+    },
+    {
+         name:"Saaho",
+         poster:"https://m.media-amazon.com/images/M/MV5BNzU0ZmFkMWUtZDc4MC00OWVmLWI0MjEtZWZjMzRkZWE4MjgyXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg",
+         rating:9.7
+    },
+    {
+         name:"Dhruva",
+         poster:"https://m.media-amazon.com/images/M/MV5BZmVhYTVmMWUtMWI4OS00NzVkLWE3ZGQtNzMwZWQxZDFjZTY1XkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg",
+         rating:8.3
+    },
+    {
+         name:"Mad",
+         poster:"https://timesofindia.indiatimes.com/photo/103442018.cms",
+         rating:9.2
+    },
+    {
+         name:"Jawaan",
+         poster:"https://scontent.fvga5-1.fna.fbcdn.net/v/t39.30808-6/289253257_4083397805217783_8829629520811285970_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=nrqccKcCrYAAX8a_LfC&_nc_ht=scontent.fvga5-1.fna&oh=00_AfDbQMWHIsxQZYhhzJt3fG3V-m0Gm6a3TaAlcaZJmQH87g&oe=654FEDB7",
+         rating:9.2
+    },
+    {
+         name:"Bro",
+         poster:"https://m.media-amazon.com/images/M/MV5BN2M2ZTM2MzMtZGMzMy00Yzc2LWEwNTUtNTI4OGJiNzYwZTUxXkEyXkFqcGdeQXVyMTUyNjIwMDEw._V1_.jpg",
+         rating:9.4
+    },
+    {
+         name:"Bheemla Nayak",
+         poster:"https://m.media-amazon.com/images/M/MV5BZmFmOTliOTQtOTBjYy00YWM3LTk2NmMtZjMyOTNhYjEyNTdhXkEyXkFqcGdeQXVyMTI1NDAzMzM0._V1_.jpg",
+         rating:9.2
+    },                   ,
+    {
+         name:"Atharintiki Daredi",
+         poster:"https://m.media-amazon.com/images/M/MV5BZWQzNDdmN2UtZTc5ZC00M2EzLWE2ZTUtMjVhNzJmOGQ5YzkwXkEyXkFqcGdeQXVyMzA0NTI2OTM@._V1_.jpg",
+         rating:9.7
+    },
+    {
+         name:"Pushpa",
+         poster:"https://m.media-amazon.com/images/M/MV5BNGZlNTFlOWMtMzUwNC00ZDdhLTk0MWUtOGZjYzFlOTBmNDdhXkEyXkFqcGdeQXVyMTUyNjIwMDEw._V1_FMjpg_UX1000_.jpg",
+         rating:9.4
+    },
+    {
+         name:"Ala Vaikuntapuramlo",
+         poster:"https://upload.wikimedia.org/wikipedia/en/2/28/Ala_Vaikunthapurramuloo.jpeg",
+         rating:8.3
+    },
+    {
+         name:"Eega",
+         poster:"https://m.media-amazon.com/images/S/pv-target-images/244951f7a45aa197f48bbf3b59e677bb675b3ca9d1478e47d9f9371d66e420f2.jpg",
+         rating:9.2
+    },
+    {
+         name:"Dasara",
+         poster:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/dasara-et00338329-1679638423.jpg",
+         rating:9.7
+    }
+
+];
+
+
+function searchMovie()
+{
+    let movieName = document.getElementById('search').value;
+    
+    if(movieName!=="")
+    {
+
+        let result =movies.filter(function(movie)
+                    {
+                        return movie.name.toUpperCase().includes(movieName.toUpperCase())
+                    })
+
+        if(result.length==0)
+        {
+            
+        }
+        displayMovies(result);
+
+
+    }
+    else 
+    {
+        displayMovies(movies);
+    }
+
+    }
+
+
+
+
+function displayMovies()
+{
+
+    let htmlString = ``;
+
+
+for(let j=0;j<movieslist.length;j++){
+    
+    htmlString=htmlString+`
+    <div class="movie">
+    <div class="overlay">
+    
+           <div class="video"></div>
+           <div class="details">
+              <h1>${movieslist[j].name}</h1>
+              <h2>${movieslist[j].rating}</h2>
+              <p>Sangeeth Shoban,Gopika Udayan</p>
+    
+           </div>
+    </div>
+                 <img class="poster" src="${movieslist[j].poster}" alt="MAD"> 
+    </div>
+    
+    </div>`
+    
+document.getElementById("movies").innerHTML=htmlString;
+}
+
+}
+
+
+
+displayMovies(movies);
+
+
+
+
+
+
+
+
